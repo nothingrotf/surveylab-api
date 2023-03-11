ee0cba9ad1ee1af7c72f3b2665db234ce810586f
nothingrotf <enderbr4@gmail.com>
2023-03-11 20:24:33 -0300

    refactor: simplify fields validations with array of required fields

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,12 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const REQUIRED_FIELDS = ['name', 'email']
+
+    for (const FIELD of REQUIRED_FIELDS) {
+      if (!httpRequest.body[FIELD]) {
+        return badRequest(new MissingParamError(FIELD))
+      }
     }
 
     return {
