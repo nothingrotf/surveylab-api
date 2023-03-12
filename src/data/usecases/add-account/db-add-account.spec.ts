cbb138ea717b5a54c5db5f16c695c3bf0af58d19
nothingrotf <enderbr4@gmail.com>
2023-03-12 18:01:42 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -109,4 +109,22 @@ describe('DbAddAccount Usecase', () => {
     const promiss = sut.add(accountData)
     await expect(promiss).rejects.toThrow()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
