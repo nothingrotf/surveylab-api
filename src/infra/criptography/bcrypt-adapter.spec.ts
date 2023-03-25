866601e90085939793441e25c1dbe4bf302c84f6
nothingrotf <enderbr4@gmail.com>
2023-03-25 11:14:52 -0300

    refactor: rename Encrypter to Hasher

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -24,13 +24,13 @@ describe('Bcrypt Adapter', () => {
     const { sut, SALT } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
 
-    await sut.encrypt('any_value')
+    await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', SALT)
   })
 
   test('Should return a hash on success', async () => {
     const { sut } = makeSut()
-    const hash = await sut.encrypt('any_value')
+    const hash = await sut.hash('any_value')
 
     expect(hash).toBe('hash')
   })
@@ -42,7 +42,7 @@ describe('Bcrypt Adapter', () => {
       throw new Error()
     })
 
-    const promise = sut.encrypt('any_value')
+    const promise = sut.hash('any_value')
 
     await expect(promise).rejects.toThrow()
   })
