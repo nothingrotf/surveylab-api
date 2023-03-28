26254d199b307c0d1e688eb2d69a779199d6e3de
nothingrotf <enderbr4@gmail.com>
2023-03-28 00:56:08 -0300

    test: ensure JwtAdapter returns a token when sign succeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return 'any_token'
+  }
+}))
+
 describe('Jwt Adapter', () => {
   test('should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
@@ -8,5 +14,11 @@ describe('Jwt Adapter', () => {
 
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('should return a token on sign succeds', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
