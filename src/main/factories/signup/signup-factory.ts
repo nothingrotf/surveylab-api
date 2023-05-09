b7127dd5d128d800372fef002bc495dfd179ec53
nothingrotf <enderbr4@gmail.com>
2023-05-09 19:56:42 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -6,13 +6,19 @@ import { SignUpController } from '../../../presentation/controllers/signup/signu
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeSignUpValidation } from './signup-validation-factory'
+import env from '../../config/env'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 
 export const makeSignUpController = (): Controller => {
   const SALT = 12
   const bcryptAdapter = new BcryptAdapter(SALT)
   const addAccountRepository = new AccountMongoRepository()
-  const addAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
-  const signUpController = new SignUpController(addAccount, makeSignUpValidation())
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(bcryptAdapter, addAccountRepository, accountMongoRepository)
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(addAccount, makeSignUpValidation(), dbAuthentication)
   const logMongoRepository = new LogMongoRepository()
 
   return new LogControllerDecorator(signUpController, logMongoRepository)
