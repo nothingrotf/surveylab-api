8e1ef00b777b83a35a5af71922884101f947f73d
nothingrotf <enderbr4@gmail.com>
2023-05-15 18:56:27 -0300

    test: ensure AccountMongoRepository returns an account on loadByToken without role success

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -25,13 +25,11 @@ describe('Account Mongo Repository', () => {
   describe('add()', () => {
     test('Should return an account on add success', async () => {
       const sut = makeSut()
-
       const account = await sut.add({
         name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password'
       })
-
       expect(account).toBeTruthy()
       expect(account.id).toBeTruthy()
       expect(account.name).toBe('any_name')
@@ -49,7 +47,6 @@ describe('Account Mongo Repository', () => {
         password: 'any_password'
       })
       const account = await sut.loadByEmail('any_email@mail.com')
-
       expect(account).toBeTruthy()
       expect(account.id).toBeTruthy()
       expect(account.name).toBe('any_name')
@@ -60,8 +57,25 @@ describe('Account Mongo Repository', () => {
     test('Should return null if loadByEmail fails', async () => {
       const sut = makeSut()
       const account = await sut.loadByEmail('any_email@mail.com')
-
       expect(account).toBeNull()
+    })
+  })
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken without role success', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
   })
 
@@ -75,13 +89,10 @@ describe('Account Mongo Repository', () => {
       })
       const fakeAccount = await accountCollection.findOne({ _id: res.insertedId })
       expect(fakeAccount?.accessToken).toBeFalsy()
-
       if (fakeAccount) {
         await sut.updateAccessToken(fakeAccount?._id.toString(), 'any_token')
       }
-
       const account = await accountCollection.findOne({ _id: fakeAccount?._id })
-
       expect(account).toBeTruthy()
       expect(account?.accessToken).toBe('any_token')
     })
