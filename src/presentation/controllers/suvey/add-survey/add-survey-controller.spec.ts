a784bfa9a0c9f909782c84f4f4d5d605827cede4
nothingrotf <enderbr4@gmail.com>
2023-05-16 00:26:39 -0300

    refactor: rename AddSurveyController test with 400 code

--- src/presentation/controllers/suvey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/suvey/add-survey/add-survey-controller.spec.ts
@@ -55,7 +55,7 @@ describe('AddSurvey Controller', () => {
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
-  test('Should return 500 if Validation fails', async () => {
+  test('Should return 400 if Validation fails', async () => {
     const { sut, validationStub } = makeSut()
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const httpResponse = await sut.handle(makeFakeRequest())
