571d1c32b1c2e1b893f45a743b704a7349f86194
nothingrotf <enderbr4@gmail.com>
2023-05-16 00:32:27 -0300

    feat: ensure LoadSurveysController returns 204 if LoadSurveys returns empty array

--- src/presentation/controllers/suvey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/suvey/load-surveys/load-surveys-controller.ts
@@ -1,4 +1,4 @@
-import { ok, serverError } from '../../../helpers/http/http-helper'
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper'
 import type { LoadSurveys } from '../../../../domain/usecases/load-surveys'
 import type { Controller, HttpRequest, HttpResponse } from '../add-survey/add-survey-controller-protocols'
 
@@ -10,6 +10,9 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
+      if (!surveys.length) {
+        return noContent()
+      }
       return ok(surveys)
     } catch (error) {
       return serverError(error)
