5c75a1f5a3c479ae1088b390b4bf37f730ee85b7
nothingrotf <enderbr4@gmail.com>
2023-05-31 23:41:07 -0300

    docs: add login api docs

---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,27 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    summary: 'Authentication user route',
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
